f99cb77651f3a30adc935f955d08958560ac7cac
hyunjeong <ahhhaaah@gmail.com>
2025-03-17 13:36:25 +0900

    git commit

---
+++ src/test/java/com/web/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
